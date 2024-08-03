2a4a3270a5cc70c55dee8995338ac9f3a88e9470
Aaron Gazzola <aaron@gazzola.dev>
2024-08-03 12:59:22 +1000

    feature(Header): improve responsive header design

--- components/Layout/Header.tsx
+++ components/Layout/Header.tsx
@@ -37,31 +37,37 @@ const Header = () => {
             Quiz.Win
           </h1>
         </Link>
-        <div className="flex items-center gap-3 flex-grow justify-center">
-          <div className="max-w-[260px] flex items-center gap-3 flex-grow justify-center">
-            <Button
-              variant="ghost"
-              className="w-full flex items-center gap-3 flex-grow justify-center h-12 px-2 sm:px-4"
-            >
-              <div className="flex flex-col items-center">
-                <span className="text-[11px] font-bold text-green-500">
-                  LEVEL
-                </span>
-                <span className="text-lg m-0 p-0 leading-none font-bold text-green-500">
-                  2
-                </span>
-              </div>
-              <div className="flex-grow rounded-lg border border-gray-500  flex items-center p-0.5">
-                <div className="w-2/3 h-0.5 rounded bg-green-500"></div>
-              </div>
-              <div
-                className={cn(
-                  "w-7 h-10 rounded-full flex flex-col items-center justify-center py-1 group-hover:dark:text-white dark:text-green-500 "
-                )}
+        <div className="flex items-center gap-3 flex-grow justify-center relative max-w-[260px] sm:max-w-[300px] overflow-hidden">
+          <div className="absolute inset-0">
+            <div className="flex items-center gap-3 flex-grow justify-start">
+              <Button
+                variant="ghost"
+                className="w-full flex items-center gap-1 sm:gap-3 flex-grow justify-start h-12 pl-2 pr-0 sm:px-4"
               >
-                <Gem className="" />
-              </div>
-            </Button>
+                <div className="flex flex-col items-center">
+                  <span className="text-[11px] font-bold text-green-500">
+                    LEVEL
+                  </span>
+                  <span className="text-lg m-0 p-0 leading-none font-bold text-green-500">
+                    2
+                  </span>
+                </div>
+                <div className="flex-grow  h-full relative overflow-hidden">
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    <div className="rounded-lg border border-gray-500  flex items-center p-0.5 w-full">
+                      <div className="w-2/3 h-0.5 rounded bg-green-500"></div>
+                    </div>
+                  </div>
+                </div>
+                <div
+                  className={cn(
+                    "w-7 h-10 rounded-full flex flex-col items-center justify-center py-1 group-hover:dark:text-white dark:text-green-500 "
+                  )}
+                >
+                  <Gem className="" />
+                </div>
+              </Button>
+            </div>
           </div>
         </div>
         <Drawer side={Direction.Right} />
