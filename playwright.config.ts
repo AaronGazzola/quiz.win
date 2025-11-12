c1cbaccb49c651574ed6c62558808781ec205886
Aaron Gazzola <aaron@gazzola.dev>
2025-11-13 09:46:30 +1100

    task(test):config

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  timeout: 120000,
   reporter: [["list"], ["./e2e/utils/consolidated-reporter.ts"]],
   outputDir: process.env.TEST_RUN_ID
     ? `test-results/${process.env.TEST_RUN_ID}`
